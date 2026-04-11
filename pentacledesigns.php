f3dabb6c505053b12f590fc7a2e1fa1f40d0d3fc
pradnya <support@technofra.com>
2026-04-11 12:02:26 +0530

    pentacel

---
+++ pentacledesigns.php
@@ -0,0 +1,201 @@
+<?php include 'header.php'; ?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-189WWHXLSS"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-189WWHXLSS');
+</script>
+<!-- MS Clarity -->
+<script type="text/javascript">
+(function(c, l, a, r, i, t, y) {
+    c[a] = c[a] || function() {
+        (c[a].q = c[a].q || []).push(arguments)
+    };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "mxzdn16ndk");
+</script>
+<!--END MS Clarity -->
+<meta name="description"
+    content="With our performance marketing services, we drive business growth. Expertise in SEM, PPC, social media ads, and more. Maximize ROI. Contact us now!">
+<title>Pentacle Case Study | Technofra</title>
+<style>
+.cover-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.cover-image img {
+    max-width: 100%;
+    height: auto;
+}
+</style>
+
+<?php include 'navbar.php'; ?>
+
+<section class="pb-60">
+    <div class="cover-image">
+        <img src="assets/image/casestudy/pentacledesigns/pentacledesigns1.webp" alt="image">
+    </div>
+</section>
+<section class="counter-with-video pb-60">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-lg-8 col-md-8">
+                <div class="section-heading">
+                    <h2>Making Healthcare Simple, Creative & Impactful</h2>
+                    <div class="space16"></div>
+                    <p>
+                        We believe everyone can be healthy. With this vision, we bring creativity into healthcare
+                        communication—making science simple, clear, and impactful. Inspired by the five natural
+                        elements, we aim to positively influence more lives every day.</p>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <a href="https://pentacledesigns.com/" target="_blank" class="btn btn-outline-info mt-20">Explore
+                    More</a>
+            </div>
+        </div>
+        <div class="col-lg-12 col-md-12">
+            <img src="assets/image/casestudy/pentacledesigns/pentacledesigns6.webp" alt="image" class="dp-r-10"
+                data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+        </div>
+    </div>
+</section>
+
+<section class="counter-with-video pb-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-md-10 ">
+                <div class="section-heading">
+                    <h2> User-Friendly Design for Mobile & Beyond </h2>
+                    <p>
+                        Our design ensures a seamless and user-friendly experience on all devices, especially mobiles.
+                        With adaptive layouts and intuitive navigation, users can enjoy effortless accessibility and
+                        optimal readability anytime, anywhere.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <img src="assets/image/casestudy/pentacledesigns/pentacledesigns2.webp" alt="image" class="dp-r-10"
+                    data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="counter-with-video pb-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-md-10 ">
+                <div class="section-heading">
+                    <h2> Responsive Design for All Devices</h2>
+                    <p>
+                        This section is designed to be responsive, ensuring optimal readability and layout across all
+                        devices, from mobile phones to desktops, with effortless accessibility for every user.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-lg-12 col-md-12">
+                <img src="assets/image/casestudy/pentacledesigns/pentacledesigns3.webp" alt="image" class="dp-r-10"
+                    data-aos="zoom-out-right">
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="counter-with-video pb-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-md-10 ">
+                <div class="section-heading">
+                    <h2>
+                        Typography & Colours
+                    </h2>
+
+                    <div class="space16"></div>
+                    <p>We've incorporated two new typefaces, Bakbak One for primary use and Gotham for secondary use,
+                        while maintaining the existing color scheme from the previous design.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-lg-12 col-md-12">
+                <img src="assets/image/casestudy/pentacledesigns/pentacledesigns4.webp" alt="image" class="dp-r-10"
+                    data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            </div>
+
+        </div>
+
+
+
+    </div>
+</section>
+<section class="counter-with-video pb-60">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6">
+                <div class="section-heading">
+
+                    <h2>
+                        Technology Used
+                    </h2>
+
+                    <div class="space16"></div>
+                    <p>This project integrates HTML, CSS, JavaScript responsive, dynamic website. Bootstrap enhances
+                        layout, Google Maps API provides location services, and Cloudinary optimizes images, ensuring
+                        seamless performance.</p>
+                </div>
+
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <img src="assets/image/casestudy/pentacledesigns/pentacledesigns7.webp" alt="image" class="dp-r-10"
+                    data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            </div>
+        </div>
+
+    </div>
+</section>
+<section class="counter-with-video pb-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-md-10 ">
+                <div class="section-heading">
+                    <h2> Empowering Our Partners to Deliver Better Care </h2>
+                    <p>
+                        As they constantly strive to push boundaries to provide the best care, we relentlessly pursue
+                        advanced, creative means to make it easy for them.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-lg-12 col-md-12">
+                <img src="assets/image/casestudy/pentacledesigns/pentacledesigns5.webp" alt="image" class="dp-r-10"
+                    data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            </div>
+
+        </div>
+    </div>
+</section>
+ 
+<?php include 'potfolio-slider.php'; ?>
+
+
+<?php include 'footer.php'; ?>
